262f572820857f7365d8e92371317b6a9f18f7ed
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-07 22:52:05 +0300

    Mongo atlas

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+const User = require( './Models/User' );
 dotenv.config();
 //#endregion
 
@@ -25,15 +27,22 @@ app.use( cors() );
 //#region Root 
 app.get('/' , (req, res)=> {
     console.log('Agoura backend!');
+    const user = new User ({ name : "AhmedMohamed"  , email : 'ahmedzain@gmail.com' , password : '059825' });
+    user.save().then(()=>{
+      console.log('Done');
+    }).catch((error)=>{
+      console.log("Error");
+    });
     res.end();
 });
 //#endregion
 
-
+//#region Database Connetion
 mongoose.connect(process.env.DATABASE);
 mongoose.connection.on("connected", () => {
   console.log("Connected to the database");
 });
+//#endregion
 
 app.listen(PORT, ()=>{
     console.log(`Server is up : http://localhost:${PORT}`);
