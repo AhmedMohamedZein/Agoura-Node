181caaa3b5d181cc0b7d8e5843957b7aa2014ff0
Ahmed Hamada <ahmedhamada.labs@gmail.com>
2023-05-08 15:52:48 +0300

    rename userController to authController

---
+++ Controllers/authController.js
@@ -0,0 +1,27 @@
+const userModel = require("../Models/User");
+const validate = require("../Utils/userValidation");
+
+class AuthController {
+  register(req, res) {
+    name = req.body.name;
+    email = req.body.email;
+    password = req.body.password;
+    confirmPassword = req.body.confirmPassword;
+
+    let newUser = new userModel({
+      name,
+      email,
+      password,
+    });
+
+    if (validate(req.body) && password === confirmPassword) {
+      newUser.save().then();
+    }
+    return res.json({
+      success: false,
+      message: "validation error",
+    });
+  }
+}
+
+module.exports = new AuthController();
