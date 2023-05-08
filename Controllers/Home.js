103267a4df35463de59adab1748e4c89933da818
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-08 21:37:36 +0300

    add try and catch for Home controller

--- Controllers/Home.js
+++ Controllers/Home.js
@@ -7,40 +7,61 @@ const notification = require(path.join(__dirname , '../Models/Notification'))
 
 const getData = async function(req , res){
 
-    const userEmail = 'ahmedzain@gmail.com' // res.auth to get  logged in user
-    const userData = await user.findOne({email: userEmail})
-    const carts = await cart.find({user:userData._id }).populate('apartments')
-    const notifications = await notification.find({user:userData._id , deleted:false})
-    const apartments = await apartment.find({})
-    // console.log(apartments)
-    const homeData = {carts , notifications , apartments  }
-    // console.log(navData)
-    await res.json(homeData)
+    try{
+        const userEmail = 'ahmedzain@gmail.com' // res.auth to get  logged in user
+        const userData = await user.findOne({email: userEmail})
+        const carts = await cart.find({user:userData._id }).populate('apartments')
+        const notifications = await notification.find({user:userData._id , deleted:false})
+        const apartments = await apartment.find({})
+        const homeData = {carts , notifications , apartments}
+        await res.status(200).json(homeData)
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
 }
 
 const deleteNotification = async function(req , res){
-    const notificationId = req.params.id
-    const notifications = await notification.updateOne({_id:notificationId} , {deleted: true})
-    console.log(notifications)
+
+    try{
+        const notificationId = req.params.id
+        const notifications = await notification.updateOne({_id:notificationId} , {deleted: true})
+        await res.status(200).json(notifications)
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
 }
 
 const deleteItemFromCart = async function(req, res) {
-    const userEmail = 'ahmedzain@gmail.com'; // res.auth to get logged in user
-    const userData = await user.findOne({ email: userEmail })
-    const itemId = req.params.id;
 
-    const item = await cart.findOneAndDeleteByItem(userData._id , itemId )
+    try{
+        const userEmail = 'ahmedzain@gmail.com'; // res.auth to get logged in user
+        const userData = await user.findOne({ email: userEmail })
+        const itemId = req.params.id;
+        const item = await cart.findOneAndDeleteByItem(userData._id , itemId )
+        await res.status(200).json(item)
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
   
-    console.log(item);
   };
 
   const addToCart = async function(req , res){
 
-    const userEmail = 'ahmedzain@gmail.com'; // res.auth to get logged in user
-    const userData = await user.findOne({ email: userEmail })
-    const itemId = req.body.id
-    const carts = await cart.findOneAndUpdateByItem(userData._id , itemId)
-    console.log(carts)
+    try{
+        const userEmail = 'ahmedzain@gmail.com'; // res.auth to get logged in user
+        const userData = await user.findOne({ email: userEmail })
+        const itemId = req.body.id
+        const carts = await cart.findOneAndUpdateByItem(userData._id , itemId)
+        await res.status(200).json(carts)
+
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+    
   }
 
 
