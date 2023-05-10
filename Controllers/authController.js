c910b69b235ba57d805b4498cc2864149e4776b4
omaralaa <omaralaa0989@gmail.com>
2023-05-10 12:18:27 +0300

    modify if  else tree in login function

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -40,6 +40,8 @@ class AuthController {
       });
   }
 
+
+//login function
   login = async (req, res) => {
     var useremail = req.body.email;
     var userPassword = req.body.password;
@@ -47,41 +49,46 @@ class AuthController {
     //check data validation
     var validateInputs = loginValidation(req.body);
        if (!validateInputs) {
+
            //if data not valid
             res.status(400).send("Data Not Valid");
-    } else {
-             //check email
-             let foundedUser = await this.checkEmail(useremail);
+            return; 
+          }  
+
+         //check email
+           let foundedUser = await this.checkEmail(useremail);
        if (!foundedUser) {
 
-             //if user doesnot exist
-              res.status(400).send("Invalid Email or Password");
-       } else {
+          //if user doesnot exist
+          res.status(400).send("Invalid Email or Password");
+          return;
+         } 
 
-               //check password
-                var checkPassword = await this.checkPassword(foundedUser.password,userPassword)
+         //check password
+         var checkPassword = await this.checkPassword(foundedUser.password,userPassword)
         if (!checkPassword) {
 
-                //if password not match
-                res.status(400).send("Invalid  Password");
-        } else {
+         //if password not match
+         res.status(400).send("Invalid  Password");
+         return;
+          } 
 
-                   //send token
-                    var token = this.sendToken(useremail);
-                    res.header("x-auth-token", token);
-                    res.status(200).send("you are logged in");
-                    // req.session.token = token;
+          //send token
+          var token = this.sendToken(useremail);
+          res.header("x-auth-token", token);
+          res.status(200).send("you are logged in");
+          // req.session.token = token;
                 }
-              }
-       } 
-  }
+        
+       
 
 
 
 
   // check user email in db
-  async checkEmail(useremail) {
-    return await userModel.findOne({ email: useremail })
+  async checkUser(useremail,hashedPassword) {
+
+    return await userModel.findOne({ email: useremail,hashedPassword })
       .then((foundedUser) => {
         if (foundedUser) {
           // Document found
@@ -96,15 +103,24 @@ class AuthController {
 
 
   //check password
-  checkPassword(foundedUserPassword, userPassword) {
-    return bcrypt
-      .compare(userPassword, foundedUserPassword)
-      .then((checkPassword) => {
-       return checkPassword;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  checkPassword(password) {
+    // Generate a salt for the hash
+bcrypt.genSalt(10, (err, salt) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Hash the password with the salt
+  bcrypt.hash(password, salt, (err, hash) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(hash); // Prints the hashed password
+  });
+});
   }
 
 
