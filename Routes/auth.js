81091672f0a3986e8f1ddb0f07e49993b3616ecf
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-10 14:38:23 +0300

    Auth route

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require("express"); 
 const router = express.Router();
-
 const authController = require("../Controllers/authController");
 
 router.post("/", authController.login);
-
+router.post("/google" , authController.newLogin);
+router.post("/facebook" , authController.newLogin);
 module.exports = router;
