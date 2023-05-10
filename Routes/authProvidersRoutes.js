231b729cbde4b3c72e33b32dcd8525ff19f43a67
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-05-10 22:00:52 +0300

    remove authproviders controller and routes

--- Routes/authProvidersRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../Controllers/authProvidersController");
-
-
-router.post("/google", authController.googleAuth);
-
-module.exports = router;
