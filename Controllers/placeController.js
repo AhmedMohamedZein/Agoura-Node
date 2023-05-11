64164ac312eff63e6a15bcd241351556c4fa5139
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-05-11 14:18:45 +0300

    add place controller

---
+++ Controllers/placeController.js
@@ -0,0 +1,10 @@
+const bidModel = require("../Models/Bid");
+const appartmentModel = require("../Models/Apartment");
+
+class PlaceController {
+  history(req, res, next) {
+    let apartmentId = req.params.id;
+  }
+}
+
+module.exports = new PlaceController();
