07aa07fb5eeaa7f9cfcd5c90478414e460969d8f
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-05-11 17:22:22 +0300

    build bidhistory api

--- Controllers/placeController.js
+++ Controllers/placeController.js
@@ -2,8 +2,39 @@ const bidModel = require("../Models/Bid");
 const appartmentModel = require("../Models/Apartment");
 
 class PlaceController {
-  history(req, res, next) {
-    let apartmentId = req.params.id;
+  async history(req, res, next) {
+    let itemId = req.params.id;
+
+    let appartment = await appartmentModel.findOne({ itemId }).populate({
+      path: "bids",
+      options: { sort: { date: -1 }, select: { _id: 1, date: 1 } },
+      populate: {
+        path: "user",
+        options: { select: "_id" },
+      },
+    });
+    let currentBid = await appartmentModel
+      .findOne({ itemId })
+      .populate({ path: "bids", options: { sort: { amountMoney: -1 } } })
+      .limit(1);
+
+    let numberOfBidders = [
+      ...new Set(currentBid.bids.map((item) => item.user.toString())),
+    ];
+
+    return res.json({
+      message: "success",
+      data: {
+        title: appartment.title,
+        description: appartment.description,
+        currentBid: currentBid.bids[0].amountMoney,
+        numberOfBids: appartment.bids.length,
+        numberOfBidders: numberOfBidders.length,
+        timeLeft: "3 hours 17 mins 12 secs",
+        duration: "7 days",
+        historyOfBids: appartment.bids,
+      },
+    });
   }
 }
 
