3a6a0ab2690e2120248952790b80a4ecac6600bc
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-13 20:43:44 +0300

    Refactoring the place route

--- Routes/place.js
+++ Routes/place.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const multer=require("multer");
 const placeConroller = require("../Controllers/placeController");
+const setupRequest = require('../Middlewares/createPlace/setupRequest');
+const uploadImage = require('../Middlewares/createPlace/uploadImage');
+const upload = multer();
+
+
 
 router.get("/:id/history", placeConroller.history);
+router.post('/create' , upload.array('photo') , setupRequest, uploadImage , placeConroller.create );
+
 
 module.exports = router;
