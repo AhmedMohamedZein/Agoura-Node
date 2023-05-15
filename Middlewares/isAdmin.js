48e4e922e2dd284aa8dd057eea322776ff540fcb
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-05-15 15:11:36 +0300

    refactor isAdmin Middleware

---
+++ Middlewares/isAdmin.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const Token = require("../Controllers/Auth/Token");
+const User = require(path.join(__dirname, "../Models/User"));
+
+isAdmin = async (req, res, next) => {
+  let token = req.headers["authorization"];
+  console.log(token);
+  if (token) {
+    user = Token.verifyToken(token);
+
+    if (user) {
+      let userData = await User.findOne({ email: user.email });
+      userData = handleData(userData);
+
+      if (userData.isAdmin) {
+        req.user = userData;
+        next();
+      }
+
+      return res.status(403).json({
+        succes: false,
+        message: "user does't have the required privilages",
+      });
+    }
+    return res.status(401).json({
+      succes: false,
+      message: "user not exist",
+    });
+  }
+
+  return res.status(401).json({
+    succes: false,
+    message: "Token not found",
+  });
+};
+
+function handleData(data) {
+  data = data.toObject();
+  delete data["password"];
+
+  return data;
+}
+
+module.exports = isAdmin;
