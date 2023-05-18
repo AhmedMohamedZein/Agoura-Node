a9020214fabeee073ff963cd15c78029a2cd71c7
omaralaa <omaralaa0989@gmail.com>
2023-05-19 02:47:37 +0300

    test google email

--- Controllers/VerifyEmail.js
+++ Controllers/VerifyEmail.js
@@ -9,15 +9,15 @@ class VERIFY_EMAIL{
       let testAccount = await nodemailer.createTestAccount();
 
       // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
+    // let transporter = nodemailer.createTransport({
+    //     service: "gmail",
        
-      // true for 465, false for other ports
-        auth: {
-            user: 'omaralaa2698@gmail.com', // generated ethereal user
-            pass: 'sdsgbxwrghumxmaq', // generated ethereal password
-        },
-    });
+    //   // true for 465, false for other ports
+    //     auth: {
+    //         user: 'omaralaa2698@gmail.com', // generated ethereal user
+    //         pass: 'sdsgbxwrghumxmaq', // generated ethereal password
+    //     },
+    // });
     
    
 
@@ -26,73 +26,73 @@ class VERIFY_EMAIL{
 
 
 
-      var message= {
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-      }
+    //   var message= {
+    //     from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    //     to: "bar@example.com, baz@example.com", // list of receivers
+    //     subject: "Hello ✔", // Subject line
+    //     text: "Hello world?", // plain text body
+    //     html: "<b>Hello world?</b>", // html body
+    //   }
     
       // send mail with defined transport object
     //   let info = await transporter.sendMail(message);
 
 
 
-      await transporter.sendMail(message).then((info) => {
-        return res.status(201).json({ 
-            msg: "you should receive an email",
-            info : info.messageId,
-            preview: nodemailer.getTestMessageUrl(info)
-        })
-    }).catch(error => {
-        // console.log(info)
-        return res.status(500).json({ error })
-    })
+    //   await transporter.sendMail(message).then((info) => {
+    //     return res.status(201).json({ 
+    //         msg: "you should receive an email",
+    //         info : info.messageId,
+    //         preview: nodemailer.getTestMessageUrl(info)
+    //     })
+    // }).catch(error => {
+    //     // console.log(info)
+    //     return res.status(500).json({ error })
+    // })
 
 
     let config = {
         service : 'gmail',
         auth : {
-            user: 'omaralaa2698@gmail.com', 
-            pass: 'sdsgbxwrghumxmaq', =
+            user: 'omaralaa0989@gmail.com', 
+            pass: 'sdsgbxwrghumxmaq', 
         }
     }
 
     let transporter = nodemailer.createTransport(config);
 
-    let MailGenerator = new Mailgen({
-        theme: "default",
-        product : {
-            name: "Mailgen",
-            link : 'https://mailgen.js/'
-        }
-    })
+    // let MailGenerator = new Mailgen({
+    //     theme: "default",
+    //     product : {
+    //         name: "Mailgen",
+    //         link : 'https://mailgen.js/'
+    //     }
+    // })
 
-    let response = {
-        body: {
-            name : "Daily Tuition",
-            intro: "Your bill has arrived!",
-            table : {
-                data : [
-                    {
-                        item : "Nodemailer Stack Book",
-                        description: "A Backend application",
-                        price : "$10.99",
-                    }
-                ]
-            },
-            outro: "Looking forward to do more business"
-        }
-    }
+    // let response = {
+    //     body: {
+    //         name : "Daily Tuition",
+    //         intro: "Your bill has arrived!",
+    //         table : {
+    //             data : [
+    //                 {
+    //                     item : "Nodemailer Stack Book",
+    //                     description: "A Backend application",
+    //                     price : "$10.99",
+    //                 }
+    //             ]
+    //         },
+    //         outro: "Looking forward to do more business"
+    //     }
+    // }
 
-    let mail = MailGenerator.generate(response)
+    // let mail = MailGenerator.generate(response)
 
     let message = {
-        from : EMAIL,
-        to : userEmail,
-        subject: "Place Order",
-        html: mail
+        from : 'omaralaa0989@gmail.com',
+        to : 'omaralaa2698@gmail.com',
+        subject: "confirm your Email",
+        html: " agora bt2olak 7la msa 3la f5adak"
     }
 
     transporter.sendMail(message).then(() => {
