5ad039da47e857967f8b363f42a99dde3d0fcbeb
omaralaa <omaralaa0989@gmail.com>
2023-05-19 03:25:11 +0300

    refactor email verifiy function

--- Controllers/VerifyEmail.js
+++ Controllers/VerifyEmail.js
@@ -1,39 +1,32 @@
 const nodemailer = require("nodemailer");
 const path = require("path");
-const user = require(path.join(__dirname , '../Models/User'));
+const user = require(path.join(__dirname, "../Models/User"));
 
+class VERIFY_EMAIL {
+  // async..await is not allowed in global scope, must use a wrapper
+  async emailSender(req, res) {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
 
-class VERIFY_EMAIL{
-
-    // async..await is not allowed in global scope, must use a wrapper
-    async emailSender(req,res) {
-      // Generate test SMTP service account from ethereal.email
-      // Only needed if you don't have a real mail account for testing
-      let testAccount = await nodemailer.createTestAccount();
-
-      // create reusable transporter object using the default SMTP transport
+    // create reusable transporter object using the default SMTP transport
     // let transporter = nodemailer.createTransport({
     //     service: "gmail",
-       
+
     //   // true for 465, false for other ports
     //     auth: {
     //         user: 'omaralaa2698@gmail.com', // generated ethereal user
     //         pass: 'sdsgbxwrghumxmaq', // generated ethereal password
     //     },
     // });
-    
-   
-
-
-
 
     let config = {
-        service : 'gmail',
-        auth : {
-            user: 'omaralaa0989@gmail.com', 
-            pass: 'sdsgbxwrghumxmaq', 
-        }
-    }
+      service: "gmail",
+      auth: {
+        user: "omaralaa0989@gmail.com",
+        pass: "sdsgbxwrghumxmaq",
+      },
+    };
 
     let transporter = nodemailer.createTransport(config);
 
@@ -65,54 +58,57 @@ class VERIFY_EMAIL{
     // let mail = MailGenerator.generate(response)
 
     let message = {
-        from : 'omaralaa0989@gmail.com',
-        to : 'omaralaa2698@gmail.com',
-        subject: "confirm your Email",
-        html: "Dear user please click the attached link to verify your email http://localhost:9000/verify/confirm "
-    }
+      from: "omaralaa0989@gmail.com",
+      to: "omaralaa2698@gmail.com",
+      subject: "confirm your Email",
+      html: "Dear user please click the attached link to verify your email http://localhost:9000/verify/confirm ",
+    };
 
-    transporter.sendMail(message).then(() => {
+    transporter
+      .sendMail(message)
+      .then(() => {
         return res.status(201).json({
-            msg: "you should receive an email"
-        })
-    }).catch(error => {
-        return res.status(500).json({ error })
-    })
+          msg: "you should receive an email",
+        });
+      })
+      .catch((error) => {
+        return res.status(500).json({ error });
+      });
+  }
 
-
-    
-    
-    }
-    
-   async emailVerifire(req,res){
+  async emailVerifire(req, res) {
     //  email="ahmedzain@gmail.com"
     //  const user = await user.findOne({email: "ahmedzain@gmail.com"})
-     user.findOneAndUpdate(
-        {email: "ahmedzain@gmail.com"}, // filter to match the user to update
-        { isVerified: true}, // update fields
-        { new: true }, // set the option to return the updated user
-        (err) => {
-          if (err) {
-            console.error(err);
-          } else {
-            res.status(200).json({
-                success: true,
-                message: "user added successfully",
-              })
-          }
-        }
-      );
-
-
-
-
-    }
-    
-
-
+    try {
+      await user.findOneAndUpdate(
+        { email: "ahmedzain@gmail.com" }, // filter to match the user to update
+        { isVerified: true }, // update fields
+        { new: true }
+      ); 
+      res.status(200).json({
+              success: true,
+              message: "user is verified",
+      });
+    } catch (err) {
+           res.status(404).json({
+                      success: false,
+                      message: "verification field",
+               });
+            }
+      }
+//     async function updateUserById(userId, updateFields) {
+//       try {
+//         const updatedUser = await User.findOneAndUpdate(
+//           { _id: userId },
+//           updateFields,
+//           { new: true }
+//         );
+//         console.log(updatedUser);
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     }
+//   }
 }
 
-
-
-
-module.exports=new VERIFY_EMAIL();
+module.exports = new VERIFY_EMAIL();
