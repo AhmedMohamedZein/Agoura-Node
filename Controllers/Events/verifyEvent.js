4e924d1c98e354754b92608d98a1b1f0bdd0a597
omaralaa <omaralaa0989@gmail.com>
2023-05-21 10:09:50 +0300

    refactor and rename verifyEvent to verifyEmailEvent

--- Controllers/Events/verifyEvent.js
+++ Controllers/Events/verifyEvent.js
@@ -2,12 +2,23 @@ const EventEmitter = require('events');
 // create a new instance of the EventEmitter class
 const myEmitter = new EventEmitter();
 
+ function verifyEvent(id,res){
 
- function verifyEvent(){
-
-myEmitter.once('register',(id,res)=>{
-
-  res.redirect(`/verify/${id}`)
-})}
+     res.redirect(`/verify/${id}`)
+     
+     }
+ myEmitter.once('register',verifyEvent)
 
 module.exports = verifyEvent;
+
+// const EventEmitter = require('events');
+
+// const myEmitter = new EventEmitter();
+
+// function onMyEvent(data) {
+//   console.log(`Received data: ${data}`);
+// }
+
+// myEmitter.on('myEvent', onMyEvent);
+
+// module.exports = myEmitter;
