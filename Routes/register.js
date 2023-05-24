f5c9c7177bd381cdb0a9f979a31fff9f947b130c
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-05-24 14:05:01 +0300

    add auth middlewares to routes

--- Routes/register.js
+++ Routes/register.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const registerController = require("../Controllers/RegisterController");
+const isGuest = require("../Middlewares/isGuest");
 
-router.post("", registerController.register);
+router.post("", isGuest, registerController.register);
 
 module.exports = router;
